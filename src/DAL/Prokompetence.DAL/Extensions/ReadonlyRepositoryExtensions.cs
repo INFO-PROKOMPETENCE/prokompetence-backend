07d7a56d4ca1d3953d0356fac83f289419c4ce8d
a.kislov <a.kislov@skbkontur.ru>
2023-03-12 22:40:46 +0500

    Add extensions methods for ReadonlyRepository

---
+++ src/DAL/Prokompetence.DAL/Extensions/ReadonlyRepositoryExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Prokompetence.DAL.Abstractions;
+
+namespace Prokompetence.DAL.Extensions;
+
+public static class ReadonlyRepositoryExtensions
+{
+    public static async Task<TEntity> GetById<TEntity, TId>(
+        this IReadonlyRepository<TEntity> source,
+        TId id,
+        CancellationToken cancellationToken
+    )
+        where TEntity : Entity<TId>
+    {
+        return await source.FindById(id, cancellationToken) ??
+               throw new Exception($"Entity with id {id} not found in database");
+    }
+
+    public static async Task<TEntity?> FindById<TEntity, TId>(
+        this IReadonlyRepository<TEntity> source,
+        TId id,
+        CancellationToken cancellationToken
+    )
+        where TEntity : Entity<TId>
+    {
+        return await source.SingleOrDefault(e => Equals(e.Id, id), cancellationToken);
+    }
+}
