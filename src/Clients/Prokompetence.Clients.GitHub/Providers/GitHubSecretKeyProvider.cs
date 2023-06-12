78e9ca89c6c099e5a30978a2ad2801d3b1497e3b
a.kislov <a.kislov@skbkontur.ru>
2023-06-12 15:32:52 +0500

    add github client

---
+++ src/Clients/Prokompetence.Clients.GitHub/Providers/GitHubSecretKeyProvider.cs
@@ -0,0 +1,23 @@
+﻿using Prokompetence.Clients.GitHub.Settings;
+
+namespace Prokompetence.Clients.GitHub.Providers;
+
+public interface IGitHubSecretKeyProvider
+{
+    Task<string> GetSecretKey();
+}
+
+public sealed class GitHubSecretKeyProvider : IGitHubSecretKeyProvider
+{
+    private readonly GitHubSettings settings;
+
+    public GitHubSecretKeyProvider(GitHubSettings settings)
+    {
+        this.settings = settings;
+    }
+
+    public async Task<string> GetSecretKey()
+    {
+        return await File.ReadAllTextAsync(settings.PathToSecretKey);
+    }
+}
---
+++ src/Clients/Prokompetence.Clients.GitHub/Providers/StringPrivateKeySource.cs
@@ -0,0 +1,18 @@
+﻿using GitHubJwt;
+
+namespace Prokompetence.Clients.GitHub.Providers;
+
+public sealed class StringPrivateKeySource : IPrivateKeySource
+{
+    private readonly string key;
+
+    public StringPrivateKeySource(string key)
+    {
+        this.key = key ?? throw new ArgumentNullException(nameof(key));
+    }
+
+    public TextReader GetPrivateKeyReader()
+    {
+        return new StringReader(key);
+    }
+}
